8da1e14ab69a587932797aea94d9abad4bee8c67
dhvl2004 <duonghoangvulam@gmail.com>
2024-10-20 17:31:31 +0800

    login and sign up

--- front end/src/pages/HomePage.tsx
+++ front end/src/pages/HomePage.tsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
 
-const HomePage = () => {
-  const [isSignedIn, setSignInState] = useState(false);
+interface UserProfile {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  industry: string;
+}
+
+interface HomeProps {
+  userProfile: UserProfile | null;
+}
+
+const HomePage = ({ userProfile }: HomeProps) => {
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  if (userProfile) {
+    setIsAuthenticated(true);
+  }
+
   return (
-    <>
-      <div>
-        <Navbar isSignedIn={isSignedIn}></Navbar>
-      </div>
-    </>
+    <div className="home-page">
+      <Navbar isSignedIn={isAuthenticated} />
+    </div>
   );
 };
 
