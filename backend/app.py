6137f7595d886a7f7c0a069b85ff4f1104fa6005
tweiyew <155275815+tweiyew@users.noreply.github.com>
2024-10-24 21:30:55 +0800

    Update app.py
    added the route to display all courses

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.auth import auth_bp
-from routes.courseroute import courses_bp
+from routes.personalisedcourses import personalisedcourses_bp
+from routes.allcourses import allcourses_bp
 from routes.profile import profile_bp
 
 app = Flask(__name__)
@@ -9,8 +10,9 @@ CORS(app)
 
 # Register blueprints
 app.register_blueprint(auth_bp)
-app.register_blueprint(courses_bp)
+app.register_blueprint(personalisedcourses_bp)
 app.register_blueprint(profile_bp)
+app.register_blueprint(allcourses_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
