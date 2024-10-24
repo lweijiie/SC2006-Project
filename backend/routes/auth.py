935625cb4458aa15ec4cc6ef394d254867c0d5ae
WJ <wjie0901@gmail.com>
2024-10-24 22:56:49 +0800

    test

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
 from models.models import UserType
-
+#test
 # Initialize the blueprint
 auth_bp = Blueprint('auth', __name__)
 bcrypt = Bcrypt()
