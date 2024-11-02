474ff9dce1f1b7fe8511d2ff082cceb20d00184d
tweiyew <weiyewteo2002@gmail.com>
2024-11-02 14:28:46 +0800

    Added endpoints for Get Internship by ID, List all internships for jobseekers

--- backend/routes/allcourses.py
+++ backend/routes/allcourses.py
@@ -1,37 +1,31 @@
 from flask import Flask, Blueprint, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, JWTManager
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from flask_jwt_extended import JWTManager
 
-# MongoDB connection URI
-mongo_uri = "mongodb+srv://SC2006:Apple12345@careerpathnow.tpgyu.mongodb.net/?retryWrites=true&w=majority&appName=CareerPathNow"
-
-# Connect to MongoDB
-database_name = 'SkillsFutureDB'
-client = MongoClient(mongo_uri)
-db = client[database_name]
-collection = db['SkillsFutureCourses']
+# Initialize the blueprint
+personalisedcourses_bp = Blueprint('personalisedcourses', __name__)
+uri = "mongodb+srv://SC2006:Apple12345@careerpathnow.tpgyu.mongodb.net/?retryWrites=true&w=majority&appName=CareerPathNow"
+client = MongoClient(uri)
+SF_courses = client.SkillsFutureDB.SkillsFutureCourses
+jobseekers = client.AppDB.jobseekers
 
 # Initialise Blueprint
 allcourses_bp = Blueprint('allcourses', __name__)
-
-# Flask app initialization
-app = Flask(__name__)
-app.config['MONGO_URI'] = mongo_uri  # Set MongoDB URI in Flask config
-mongo = PyMongo(app)
 
 
 @allcourses_bp.route('/api/courses', methods=['GET'])
 @jwt_required()
 def get_courses():
     # Fetch courses from the database
-    courses = mongo.db.SkillsFutureCourses.find()  # Your collection name
-    courses_list = list(courses)  # Convert cursor to list
+    courses = SF_courses.find()
+    courses_list = []
 
-    # Optionally format the courses to exclude certain fields
-    for course in courses_list:
+    # Convert each document to a dictionary and format _id
+    for course in courses:
         course['_id'] = str(course['_id'])  # Convert ObjectId to string
+        courses_list.append(course)
 
     return jsonify(courses_list), 200  # Return JSON response
 
