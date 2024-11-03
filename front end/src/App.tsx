7b5372314cc47b20ca58342cb6c8ddc07429c34a
dhvl2004 <duonghoangvulam@gmail.com>
2024-11-03 14:54:35 +0800

    .

--- front end/src/App.tsx
+++ front end/src/App.tsx
@@ -11,6 +11,7 @@ const App: React.FC = () => {
     firstName: "",
     lastName: "",
     industry: "",
+    education: "",
   });
 
   const [employerProfile, setEmployerProfile] = useState<EmployerData>({
