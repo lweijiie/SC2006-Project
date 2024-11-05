99ce8d4ef982bee193a6e10ee0de884f146d6a53
tweiyew <weiyewteo2002@gmail.com>
2024-11-05 17:18:07 +0800

    Employer profile page updated, to include access token function. However, more edits are needed for the displaying of pages so that it is more logical

--- frontend/src/services/FetchEmployerProfile.tsx
+++ frontend/src/services/FetchEmployerProfile.tsx
@@ -2,30 +2,34 @@ import { API_BASE_URL } from "../constants";
 import { EmployerData } from "../store/auth/interface";
 
 const FetchEmployerProfile = async (
-  userId: string,
-  access_token: string
+  userId: string | null,
+  access_token: string | null
 ): Promise<EmployerData> => {
   try {
-    const response = await fetch(`${API_BASE_URL}/employer-profile/${userId}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      `${API_BASE_URL}/employer-profile/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (!response.ok) {
       const errorData = await response.json();
-      throw new Error(errorData.message || "Failed to fetch user profile.");
+      throw new Error(errorData.message || "Failed to fetch employer profile.");
     }
 
     const data = await response.json();
+
     return {
       _id: data.user._id,
       email: data.user.email,
-      companyName: data.user.companyName,
       industry: data.user.industry,
-      companyDescription: data.user.companyDescription,
+      companyName: data.user.company_name,
+      companyDescription: data.user.company_description,
     };
   } catch (err: any) {
     throw new Error(
