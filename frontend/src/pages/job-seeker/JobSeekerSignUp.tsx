8768eaa633e949c38eea429dfabc52babc292964
yaosheng <yaosheng2001@gmail.com>
2024-11-10 18:43:33 +0800

    did up sign up page

--- frontend/src/pages/job-seeker/JobSeekerSignUp.tsx
+++ frontend/src/pages/job-seeker/JobSeekerSignUp.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import JobSeekerSignUpForm from "../../components/Form/SignUpForm/JobSeekerSignUpForm";
 import GoBackButton from "../../components/Button/GoBackButton";
+import "../../pages/styles/LoginPage.css";
+import Logo from "../../components/Logo/Logo";
 
 const JobSeekerSignUp: React.FC = () => {
   return (
-    <div>
-      <GoBackButton />
-      <JobSeekerSignUpForm />
+    <div className="login-container">
+      <div className="login-form-section">
+        <div className="logo-container">
+          <Logo />
+        </div>
+        <div className="back-button">
+          <GoBackButton />
+        </div>
+        <JobSeekerSignUpForm />
+      </div>
     </div>
   );
 };
